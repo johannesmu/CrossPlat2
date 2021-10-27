6ea75fd63e4e4472a9cee3947254da8d28c68b8b
Rica <ricamaeaverion22@gmail.com>
2021-10-27 11:38:26 +1100

    added components, navigation stack and firebase

---
+++ Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAUBGKoorBmz1xM5wUdfvIWtm8U8o8JBWo",
+    authDomain: "crossplatform-a93aa.firebaseapp.com",
+    projectId: "crossplatform-a93aa",
+    storageBucket: "crossplatform-a93aa.appspot.com",
+    messagingSenderId: "852768179229",
+    appId: "1:852768179229:web:12147ca59ad7fa8cfdf2f4",
+    measurementId: "G-K00R6ZQ17Q"
+  }
--- components/Signup.js
+++ components/Signup.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import {useNavigation} from '@react-navigation/native';
+
 
 export function Signup (props){
+
+    const navigation = useNavigation()
+
     return(
         <View>
             <Text>SIGN UP</Text>
+            <Button title="Click here to sign in" onPress={ () => navigation.navigate('Signin')} />
         </View>
     ) 
 }
