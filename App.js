db1e850e65033583cd461b7164919c0e37bd9752
Rica <ricamaeaverion22@gmail.com>
2021-11-03 10:43:58 +1100

    sign up form validation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
