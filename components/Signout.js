2b1f378cfc803759b38c95ec5b7878848048307d
Rica <ricamaeaverion22@gmail.com>
2021-11-09 22:28:11 +1100

    added SignOut component

---
+++ components/Signout.js
@@ -0,0 +1,9 @@
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+export function Signout() {
+  return(
+    <TouchableOpacity>
+      <Text>Signout</Text>
+    </TouchableOpacity>
+  )
+}
