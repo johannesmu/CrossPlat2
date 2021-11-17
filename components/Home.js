057e23e2e732e8fc5215f7cf7650f964df136523
Rica <ricamaeaverion22@gmail.com>
2021-11-17 11:37:16 +1100

    signout bug fixes

--- components/Home.js
+++ components/Home.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from "react-native";
-import { Signout } from './Signout';
 import { useNavigation } from '@react-navigation/native';
 
 export function Home (props){
     const navigation = useNavigation()
 
     useEffect( () => {
-        navigation.setOptions( {
-            headerRight: props => <Signout {...props} />
-        })
-    })
+        if(!props.auth){
+            navigation.reset({ index: 0, routes: [ {name: 'Signin'} ] })
+        }
+    }, [props.auth])
 
 
     return(
         <View>
             <Text>HOME</Text>
-            <Signout />
         </View>
     ) 
 }
