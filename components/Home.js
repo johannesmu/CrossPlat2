61120121450c479b043e7759393cd1e65b65f1f5
Rica <ricamaeaverion22@gmail.com>
2021-11-26 11:00:33 +1100

    fixed Home code

--- components/Home.js
+++ components/Home.js
@@ -1,56 +1,65 @@
 // Imports
-import React, {useEffect,useState} from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, TextInput } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import { ThemeColours } from './ThemeColours';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { ThemeColours } from "./ThemeColours";
 
-import { ListItem } from './ListItem';
-import Constants from 'expo-constants';
-import { SearchBar } from 'react-native-elements';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faPlusCircle, faCheckSquare, faSortAmountDown, faSortAmountUp } from '@fortawesome/free-solid-svg-icons';
+import { ListItem } from "./ListItem";
+import Constants from "expo-constants";
+import { SearchBar } from "react-native-elements";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faPlusCircle,
+  faCheckSquare,
+  faSortAmountDown,
+  faSortAmountUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-export function Home ( props ) {
-
-  const navigation = useNavigation()
-  const [ listData, setListData ] = useState()
+export function Home(props) {
+  const navigation = useNavigation();
+  const [listData, setListData] = useState();
 
   // Go to sign in
-  useEffect( () => {
-   if(!props.auth) {
-    navigation.reset({ index: 0, routes: [ {name: 'Signin'} ] })
-   }
-   console.log(props.user)
-  }, [props.auth])
+  useEffect(() => {
+    if (!props.auth) {
+      navigation.reset({ index: 0, routes: [{ name: "Signin" }] });
+    }
+    console.log(props.user);
+  }, [props.auth]);
 
   // Get data
-  useEffect( () => {
-    setListData(props.data)
-  }, [props.data])
+  useEffect(() => {
+    setListData(props.data);
+  }, [props.data]);
 
-  const data = { time: new Date().getTime(), user: Math.random() * 100 }
+  const data = { time: new Date().getTime(), user: Math.random() * 100 };
 
-  const renderItem = ({item}) => (
+  const renderItem = ({ item }) => (
     <View>
-      <Text>
-        {item.time}
-      </Text>
+      <Text>{item.time}</Text>
     </View>
-  )
+  );
 
   // CODE FROM PREVIOUS PROTOTYPE
 
   //const [data, setData] = useState([])
-  const [validInput, setValidInput] = useState(false)
-  const [input, setInput] = useState()
-  const [appInit, setAppInit] = useState(true)
-  const [sortAsc, setSortAsc] = useState(false)
+  const [validInput, setValidInput] = useState(false);
+  const [input, setInput] = useState();
+  const [appInit, setAppInit] = useState(true);
+  const [sortAsc, setSortAsc] = useState(false);
 
   // useEffect ( () => {
   //   if(appInit){
   //     getData()
   //     setAppInit(false)
-  //   } 
+  //   }
   //   else {
   //     storeData()
   //   }
@@ -58,44 +67,44 @@ export function Home ( props ) {
 
   // Validate input
   const onTextChange = (value) => {
-    setInput(value)
-    if(value.length > 0) { 
-      setValidInput(true)
+    setInput(value);
+    if (value.length > 0) {
+      setValidInput(true);
     } else {
-      setValidInput(false)
+      setValidInput(false);
     }
-  }
+  };
 
   // Add new task
   const onSubmit = () => {
-    const id = new Date().getTime().toString()
-    const item = {id: id, name: input, status: false}
-    setData([...data, item])
-    setInput(null)
-    setValidInput(false)
-  }
+    // const id = new Date().getTime().toString()
+    // const item = {id: id, name: input, status: false}
+    // setData([...data, item])
+    // setInput(null)
+    // setValidInput(false)
+  };
 
   // Delete task
   const onDelete = (id) => {
-    let items = [...data]
-    let newData = items.filter( (item) => {
-      if(item.id !== id){
-        return item
-      }
-    })
-    setData(newData)
-  }
+    // let items = [...data]
+    // let newData = items.filter( (item) => {
+    //   if(item.id !== id){
+    //     return item
+    //   }
+    // })
+    // setData(newData)
+  };
 
   // Set task's status (done/not done)
   const changeStatus = (id) => {
-    let items = [...data]
-    items.forEach( (item) => {
-      if(item.id === id){
-        item.status = !item.status
+    let items = [...data];
+    items.forEach((item) => {
+      if (item.id === id) {
+        item.status = !item.status;
       }
-    })
-    setData(items)
-  }
+    });
+    setData(items);
+  };
 
   // // Retain data
   // const storeData = async() => {
@@ -119,74 +128,122 @@ export function Home ( props ) {
 
   // Sort FlatList
   const sortData = () => {
-    let items = [...data]
-    if(sortAsc){
-      items.sort( (item1, item2) => {
-            return item1.id - item2.id
-          })
+    let items = [...data];
+    if (sortAsc) {
+      items.sort((item1, item2) => {
+        return item1.id - item2.id;
+      });
+    } else {
+      items.sort((item1, item2) => {
+        return item2.id - item1.id;
+      });
     }
-    else {
-      items.sort( (item1, item2) => {
-        return item2.id - item1.id
-      })
-    }
-    setData(items)
-    setSortAsc(!sortAsc)
-  }
+    setData(items);
+    setSortAsc(!sortAsc);
+  };
 
   // Delete all tasks marked as completed
   const deleteAllCompleted = () => {
-    let items = [...data]
-    let newItems = items.filter( (item) => {
-      if (item.status == false){
-        return item
+    let items = [...data];
+    let newItems = items.filter((item) => {
+      if (item.status == false) {
+        return item;
       }
-    })
-    setData(newItems)
-  }
+    });
+    setData(newItems);
+  };
 
   // FlatList's items
-  const Renderer = ({item}) => (
-    <Item 
-      text={item.name} 
-      delete={onDelete} 
-      id={item.id} 
+  const Renderer = ({ item }) => (
+    <ListItem
+      text={item.name}
+      delete={onDelete}
+      id={item.id}
       status={item.status}
-      done={changeStatus} />)
+      done={changeStatus}
+    />
+  );
 
   // Sort buttons code
   const SortButton = () => {
-    if(sortAsc){
-      return(
+    if (sortAsc) {
+      return (
         // Wrapping the icons in TouchableOpacity so they're easier to click on
         <TouchableOpacity>
-          <FontAwesomeIcon 
-            icon={ faSortAmountUp } 
-            size={25} 
-            style={{color:'#3d5187'}}
+          <FontAwesomeIcon
+            icon={faSortAmountUp}
+            size={25}
+            style={{ color: "#3d5187" }}
             onPress={sortData}
           />
         </TouchableOpacity>
-      )
-    }
-    else {
-      return(
+      );
+    } else {
+      return (
         <TouchableOpacity>
-          <FontAwesomeIcon 
-              icon={ faSortAmountDown } 
-              size={25} 
-              style={{color:'#3d5187'}}
-              onPress={sortData}
-            />
+          <FontAwesomeIcon
+            icon={faSortAmountDown}
+            size={25}
+            style={{ color: "#3d5187" }}
+            onPress={sortData}
+          />
         </TouchableOpacity>
-      )
+      );
     }
-  }
+  };
 
-  return(
-    <View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.listContainer}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Your Tasks</Text>
+          <SortButton />
+        </View>
+        <SearchBar
+          lightTheme
+          placeholder="Search"
+          containerStyle={styles.searchContainerStyle}
+          inputContainerStyle={{ backgroundColor: "white", fontSize: 16 }}
+          // onChangeText={this.updateSearch}
+          // value={search}
+        />
+        <View>
+          <FlatList
+            data={data}
+            // keyExtractor={ (item) => item.id }
+            // renderItem={Renderer}
+          />
+        </View>
+
+        <View style={styles.addTask}>
+          <FontAwesomeIcon
+            style={{ color: "lightgrey" }}
+            icon={faPlusCircle}
+            size={25}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onTextChange}
+            placeholder="New task"
+            value={input}
+            placeholderTextColor="silver"
+          />
+          <FontAwesomeIcon
+            icon={faCheckSquare}
+            size={35}
+            //onPress={onSubmit}
+            style={validInput ? styles.icon : styles.iconDisabled}
+            disabled={validInput ? false : true}
+          />
+        </View>
+      </View>
+      <View style={styles.deleteAllContainer}>
+        <TouchableOpacity onPress={deleteAllCompleted}>
+          <Text style={styles.deleteAllText}>Delete all completed</Text>
+        </TouchableOpacity>
+      </View>
+
       <View>
-        <Text></Text>
         <TouchableOpacity 
           style={styles.button} 
           onPress={ () => { props.add('users', data ) }}>
@@ -194,59 +251,8 @@ export function Home ( props ) {
         </TouchableOpacity>
         <FlatList data={listData} renderItem={renderItem} keyExtractor={item => item.id} />
       </View>
-      
-      <View>
-        <View style={styles.container }>
-          <View style={styles.listContainer} >
-            <View style={styles.header} >
-              <Text style={styles.headerText} >Your Tasks</Text>
-              <SortButton />
-            </View>
-            <SearchBar
-              lightTheme
-              placeholder="Search"
-              containerStyle={styles.searchContainerStyle}
-              inputContainerStyle={{backgroundColor:'white', fontSize:16}}
-              // onChangeText={this.updateSearch}
-              // value={search}
-            />
-            <View>
-              <FlatList 
-                data={data} 
-                keyExtractor={ (item) => item.id } 
-                renderItem={Renderer} />
-            </View>
-
-            <View style={styles.addTask} >
-                  <FontAwesomeIcon 
-                    style={{color:'lightgrey'}} 
-                    icon={ faPlusCircle } 
-                    size={25}
-                  />
-                  <TextInput 
-                    style={ styles.input } 
-                    onChangeText={onTextChange}
-                    placeholder="New task"
-                    value={input}
-                    placeholderTextColor='silver' />
-                  <FontAwesomeIcon 
-                    icon={ faCheckSquare } 
-                    size={35} 
-                    onPress={onSubmit}
-                    style={ (validInput) ? styles.icon : styles.iconDisabled } 
-                    disabled={ (validInput) ? false : true }
-                  />
-            </View>
-          </View>
-          <View style={styles.deleteAllContainer}>
-            <TouchableOpacity onPress={deleteAllCompleted} >
-              <Text style={styles.deleteAllText} >Delete all completed</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -255,49 +261,48 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 
-
   container: {
     padding: 15,
     flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#aebee8',
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#aebee8",
     marginTop: Constants.statusBarHeight,
   },
   listContainer: {
-    flex: 1
+    flex: 1,
   },
   // Header
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   headerText: {
-    color: '#3d5187',
+    color: "#3d5187",
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     flex: 1,
     marginTop: 20,
     marginBottom: 20,
   },
   // Search bar
   searchContainerStyle: {
-    backgroundColor:'transparent',
+    backgroundColor: "transparent",
     padding: 0,
     marginBottom: 10,
   },
   // Add new task
   addTask: {
     padding: 15,
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: '#3d5187',
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#3d5187",
     borderRadius: 10,
     marginTop: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   input: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
     padding: 5,
     flex: 1,
@@ -305,21 +310,21 @@ const styles = StyleSheet.create({
     marginRight: 15,
   },
   icon: {
-    color:'lightblue',
+    color: "lightblue",
   },
   iconDisabled: {
-    color: 'grey',
+    color: "grey",
   },
   // Delete all button
   deleteAllContainer: {
-    backgroundColor: 'tomato',
+    backgroundColor: "tomato",
     borderRadius: 10,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginBottom: 40,
   },
   deleteAllText: {
     fontSize: 16,
-    color: 'white',
+    color: "white",
     padding: 15,
-  }
-})
+  },
+});
