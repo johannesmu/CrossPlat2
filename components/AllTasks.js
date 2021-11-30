e60227ff4b37744db06092e2fc37898fa58064c4
Rica <ricamaeaverion22@gmail.com>
2021-11-30 14:17:34 +1100

    AllTask UI update

--- components/AllTasks.js
+++ components/AllTasks.js
@@ -7,7 +7,7 @@ export function AllTasks() {
     <View style={styles.container}>
       <Text style={styles.headerText}>All tasks</Text>
       <View style={styles.view}>
-        <View style={styles.row}>
+        <View style={styles.rowMiddle}>
           <Text style={styles.text}>On-going Tasks</Text>
           <Text style={styles.text}>#</Text>
         </View>
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
   },
-  row: {
+  rowMiddle: {
     paddingBottom: 10,
     marginBottom: 10,
     display: "flex",
@@ -55,6 +55,11 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     borderBottomWidth: 1,
     borderBottomColor: "grey",
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   text: {
     fontSize: 16,
