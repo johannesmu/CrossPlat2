833478573e28e95446a2bcc81ac9baba45774922
johannes <jmuljana@gmail.com>
2021-12-06 11:24:54 +1100

    rework splash to be auth aware

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useNavigation } from "@react-navigation/native";
 import { ImageBackground } from "react-native";
 
-export function SplashScreen() {
+export function SplashScreen( props ) {
+  const navigation = useNavigation()
+
+  useEffect( () => {
+    // set screenName depending on authentication state
+    const screenName = ( props.auth ) ? "TabNavigation" : "Signup"
+    // timer to change the route to Signup after a certain amount of time
+    const timer = setTimeout( () => { 
+      navigation.reset({ index: 0, routes: [{ name: "TabNavigation" }] });
+    }, parseFloat(props.time) )
+  }, [props.auth] )
+
+
   return (
     <ImageBackground
       source={require("../resources/splash.png")}
